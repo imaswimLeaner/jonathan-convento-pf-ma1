aeb03c09665f8e8e13dd09fd475a4fa063dcafcd
imaswimLeaner <61804928+imaswimLeaner@users.noreply.github.com>
2020-09-20 22:06:38 +0200

    ma1-js-jonathan

---
+++ ca-js1/script.js
@@ -0,0 +1,62 @@
+// question 
+//1 Declare a variable called firstVar but don't initialise it with a value.
+
+//Answer
+
+var firstVar = 35;
+console.log(firstVar)
+
+//question 
+//2 Declare a variable called name and assign it your first name.
+
+// Answer
+var name = "Jonathan";
+console.log(name)
+
+//question
+//3 Declare and initialise a variable with a number value.
+var name = "Jonathan";
+console.log(name)
+console.log(name.length)
+
+//question
+//4 Create a variable called division and initialise it with a value of 20 divided by 5.
+
+var value = 20 / 5;
+console.log(value)
+
+//question 
+//5 Write code that checks the type of the value "frog".
+
+var animal =["frog"];
+console.log(animal[0]);
+
+//question
+//6 Declare and initialise a variable called orderHasShipped with a boolean value.
+
+var orderHasShipped = true;
+
+//question 
+//7 Create an if statement that checks if orderHasShipped is true. If it is true, console log the string value "true". If not, console log the string value "false".
+
+if (orderHasShipped = true) {
+    console.log("Your order has shipped!");
+
+}
+else {
+    console.log ("Your order has not shipped yet..")
+}
+
+
+//question 
+// 8 Create a for loop that counts from 0 to 9. Console log the value of the counter variable inside the loop.
+
+for (var i = 0; i <= 9; i += 1) {
+    console.log(i);
+  }
+
+
+
+
+
+
